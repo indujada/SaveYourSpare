3bc840d1c0fd02708fa1000114550e00f02b75e4
Indu <indupriya454@gmail.com>
2019-02-23 19:46:05 -0800

    Hack2Hire SaveUsingSpare

--- app/src/main/java/com/dbs/hack2hire/saveyourspare/model/TransactionDetails.java
+++ app/src/main/java/com/dbs/hack2hire/saveyourspare/model/TransactionDetails.java
@@ -3,9 +3,76 @@ package com.dbs.hack2hire.saveyourspare.model;
 import com.google.gson.annotations.SerializedName;
 
 public class TransactionDetails {
-    @SerializedName("userName")
-    private String userName = "";
 
-    @SerializedName("passWord")
-    private String passWord = "";
+    @SerializedName("transactionDate")
+    private String transactionDate = "";
+
+    @SerializedName("fromAccount")
+    private String fromAccount = "";
+
+    @SerializedName("toAccount")
+    private String toAccount = "";
+
+    @SerializedName("amountTransferred")
+    private String amountTransferred = "";
+
+    @SerializedName("savingsDone")
+    private String savingsDone = "";
+
+    @SerializedName("category")
+    private String category = "";
+
+    public TransactionDetails(){
+
+    }
+
+    public TransactionDetails(String fromAccount, String toAccount, String amountTransferred, String savingsDone,
+                              String category, String transactionDate) {
+        this.fromAccount = fromAccount;
+        this.toAccount = toAccount;
+        this.amountTransferred = amountTransferred;
+        this.savingsDone = savingsDone;
+        this.category = category;
+        this.transactionDate = transactionDate;
+    }
+
+    public String getFromAccount() {
+        return fromAccount;
+    }
+
+    public void setFromAccount(String fromAccount) {
+        this.fromAccount = fromAccount;
+    }
+
+    public String getToAccount() {
+        return toAccount;
+    }
+
+    public void setToAccount(String toAccount) {
+        this.toAccount = toAccount;
+    }
+
+    public String getAmountTransferred() {
+        return amountTransferred;
+    }
+
+    public void setAmountTransferred(String amountTransferred) {
+        this.amountTransferred = amountTransferred;
+    }
+
+    public String getSavingsDone() {
+        return savingsDone;
+    }
+
+    public void setSavingsDone(String savingsDone) {
+        this.savingsDone = savingsDone;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
--- app/src/main/java/com/dbs/hack2hire/saveyourspare/presenter/TransactionsPresenter.java
+++ app/src/main/java/com/dbs/hack2hire/saveyourspare/presenter/TransactionsPresenter.java
@@ -1,8 +1,17 @@
 package com.dbs.hack2hire.saveyourspare.presenter;
 
+import com.dbs.hack2hire.saveyourspare.model.TransactionDetails;
+
 public class TransactionsPresenter {
+    TransactionsView transactionsView;
+    TransactionDetails transactionDetails;
 
+    public TransactionsPresenter(TransactionsView transactionsView) {
+        this.transactionsView = transactionsView;
+        transactionDetails = new TransactionDetails();
+    }
 
+    public interface TransactionsView{
 
+    }
 }
-
